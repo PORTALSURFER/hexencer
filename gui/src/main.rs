4772536d970433f8dff3abd0418e587b6aacf095
PORTALSURFER <portalsurfer@gmail.com>
2024-05-23 08:28:19 +0200

    added core lib

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -6,39 +6,62 @@ fn main() {
         ..Default::default()
     };
 
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
-
     eframe::run_native(
         "Hexencer",
         options,
         Box::new(|cc| Box::new(Hexencer::new(cc))),
-    );
+    )
+    .expect("failed to start eframe app");
 }
 
+struct TrackManager {
+    tracks: Vec<Track>,
+}
+
+struct InstrumentManager {
+    struments: Vec<Instrument>,
+}
+struct ProjectManager {
+    track_manager: TrackManager,
+    instrument_manager: InstrumentManager,
+}
 #[derive(Default)]
 struct Hexencer {}
 
 impl Hexencer {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
     }
 }
 
+const TRACK_HEIGHT: f32 = 25.0;
+
 impl eframe::App for Hexencer {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::top("toolbar").show(ctx, |ui| {
             ui.label("some toolbar");
         });
         egui::SidePanel::left("info").show(ctx, |ui| {
             ui.label("info");
         });
-        egui::CentralPanel::default().show(ctx, |ui| {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                ui.vertical(|ui| {
-                    ui.heading("sequencer");
-                })
+        egui::CentralPanel::default().show(ctx, |_ui| {
+            egui::CentralPanel::default().show(ctx, |_ui| {
+                for i in 0..10 {
+                    track(ctx, i);
+                }
+                egui::CentralPanel::default().show(ctx, |_ui| {});
             });
         });
     }
+}
+
+fn track(ctx: &egui::Context, index: u32) {
+    egui::TopBottomPanel::top(format!("tracklane_{}", index))
+        .min_height(TRACK_HEIGHT)
+        .show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.add(egui::Label::new(format!("track {}", index)).selectable(false));
+                ui.button("X");
+            });
+        });
 }
