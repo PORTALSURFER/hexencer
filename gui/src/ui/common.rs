9d501fb6b0de89acd7d890b83c4ea3b97eef719a
PORTALSURFER <portalsurfer@gmail.com>
2024-05-26 12:58:24 +0200

    added custom track container to get control of height

---
+++ gui/src/ui/common.rs
@@ -0,0 +1,4 @@
+pub const TRACK_HEIGHT: f32 = 20.0;
+pub const TRACK_HEADER_WIDTH: f32 = 150.0;
+pub const TRACK_COLOR: egui::Color32 = egui::Color32::from_rgb(32, 42, 42);
+pub const TRACK_HEADER_COLOR: egui::Color32 = egui::Color32::from_rgb(54, 54, 74);
