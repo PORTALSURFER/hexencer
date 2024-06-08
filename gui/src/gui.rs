3cafec03bb941840601e7ff1e9a1315185884306
PORTALSURFER <portalsurfer@gmail.com>
2024-06-08 17:35:39 +0200

    basic command path added using app context

--- gui/src/gui.rs
+++ gui/src/gui.rs
@@ -7,6 +7,8 @@ use hexencer_core::data::DataInterface;
 use hexencer_engine::SequencerSender;
 
 pub use self::core::HexencerApp;
+pub use self::core::HexencerContext;
+pub use self::core::SystemCommand;
 
 /// color used for all regular edges in the ui
 pub const EDGE_COLOR: Color32 = Color32::from_rgb(20, 20, 20);
@@ -16,7 +18,13 @@ pub fn run(options: NativeOptions, data_layer: DataInterface, sequencer_sender: 
     eframe::run_native(
         "Hexencer",
         options,
-        Box::new(|_cc| Box::new(HexencerApp::new(data_layer, sequencer_sender))),
+        Box::new(|cc| {
+            Box::new(HexencerApp::new(
+                data_layer,
+                sequencer_sender,
+                cc.egui_ctx.clone(),
+            ))
+        }),
     )
     .expect("failed to start eframe app");
 }
