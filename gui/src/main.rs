ca88b002340a866b409a8810892bf4686d030adc
PORTALSURFER <PORTALSURFER@users.noreply.github.com>
2024-06-22 21:54:36 +0200

    seems most is working again now?

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,14 +1,21 @@
-use iced::advanced::graphics::color;
-use iced::advanced::layout::{self, Layout};
-use iced::advanced::widget::{self, Widget};
-use iced::widget::center;
+//! the main entry point for the application
+
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+
+mod widget;
+
+use hexencer_core::data::{ClipId, StorageInterface};
+use hexencer_core::{Tick, TrackId};
 use iced::widget::horizontal_space;
+use iced::widget::{center, text};
 use iced::widget::{column, container, row, scrollable};
 use iced::Alignment;
-use iced::{mouse, Font};
-use iced::{Element, Length, Rectangle, Renderer, Size, Theme, Transformation, Vector};
+use iced::Font;
+use iced::{Element, Length, Theme};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+use widget::{Clip, DragEvent, Track};
 
 fn main() {
     info!("start gui");
@@ -30,16 +37,83 @@ pub fn init_logger() {
     tracing::info!("hexencer started");
 }
 
-#[derive(Debug, Clone)]
-enum Message {}
+/// Message enum for the application
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    /// exit this application
+    Exit,
+    /// global message for dragging a clip
+    DragClip {
+        /// the clip id of the clip being dragged
+        clip_id: ClipId,
+        /// the point where the mouse click started the drag
+        origin: f32,
+    },
+    /// a clip was dropped
+    DroppedClip {
+        /// the clip id of the clip being dragged
+        clip_id: ClipId,
+    },
+    /// request to move clip between tracks
+    MoveClipRequest {
+        /// the id of the clip to move
+        clip_id: ClipId,
+        /// the new position of the clip
+        track_id: TrackId,
+        /// tick where the clip should be placed
+        cursor_position: f32,
+    },
+}
 
-#[derive(Default, Debug, Clone)]
+#[derive(Debug, Clone)]
 struct Hexencer {
     theme: Theme,
+    storage: StorageInterface,
+    dropped_clip: Option<ClipId>, // TODO move this elsewhere
+    drag_origin: f32,
+}
+
+impl Default for Hexencer {
+    fn default() -> Self {
+        Self {
+            theme: Theme::KanagawaDragon,
+            storage: StorageInterface::default(),
+            dropped_clip: None,
+            drag_origin: 0.0,
+        }
+    }
 }
 
 impl Hexencer {
-    fn update(&mut self, message: Message) {}
+    fn update(&mut self, message: Message) {
+        if let Some(dropped_clip) = self.dropped_clip {
+            tracing::info!("dropped clip: {:?}", dropped_clip);
+            self.dropped_clip = None;
+        }
+        match message {
+            Message::Exit => std::process::exit(0),
+            Message::DragClip { origin, .. } => {
+                self.drag_origin = origin;
+                self.dropped_clip = None;
+            }
+            Message::DroppedClip { clip_id } => {
+                self.dropped_clip = Some(clip_id);
+            }
+            Message::MoveClipRequest {
+                clip_id,
+                track_id,
+                cursor_position,
+            } => {
+                info!("move clip request: {:?} to {:?}", clip_id, track_id);
+                let tick = Tick::from(cursor_position - self.drag_origin);
+                self.storage
+                    .write()
+                    .unwrap()
+                    .project_manager
+                    .move_clip(clip_id, track_id, tick);
+            }
+        }
+    }
 
     fn view(&self) -> Element<Message> {
         let header = container(
@@ -48,15 +122,72 @@ impl Hexencer {
                 .align_items(Alignment::Center),
         );
 
+        let bpm = self.storage.read().unwrap().bpm();
+        let bpm_widget = text(bpm.to_string()).size(60);
+
         let bottom = container(
-            row![horizontal_space(), "Bottom", horizontal_space()]
+            row![horizontal_space(), bpm_widget, horizontal_space()]
                 .padding(10)
                 .align_items(Alignment::Center),
         );
 
+        let mut elements = Vec::new();
+        let data = self.storage.read().unwrap();
+        let track_list = &data.project_manager.tracks;
+
+        for (index, track) in track_list.iter().enumerate() {
+            let clips = &track.clips;
+            let mut clip_elements = Vec::new();
+
+            for (clip_id, _clip) in clips.iter() {
+                let clip_key = clip_id.clone();
+                let clip_element = Clip::new(
+                    clip_key.id,
+                    &self.storage,
+                    text("drag drop system in place...yay"),
+                )
+                .on_drag(move |drag_event| {
+                    let mut origin = 0.0;
+                    if let DragEvent::DragStarted { grab_position } = drag_event {
+                        info!("clip drag started at {:?}", grab_position);
+                        origin = grab_position;
+                    }
+                    Message::DragClip {
+                        clip_id: clip_key.id,
+                        origin,
+                    }
+                })
+                .on_drop(move |_| Message::DroppedClip {
+                    clip_id: clip_key.id,
+                });
+                clip_elements.push(clip_element.into());
+            }
+            let track_id = track.id;
+
+            let track = Track::new(
+                &self.storage,
+                index,
+                track_id,
+                clip_elements,
+                self.dropped_clip,
+            )
+            .on_drop(move |clip_id, track_id, cursor_position| {
+                info!("dropped clip: {:?} onto track {}", clip_id, index);
+                Message::MoveClipRequest {
+                    clip_id,
+                    track_id,
+                    cursor_position,
+                }
+            });
+            elements.push(track.into());
+        }
+
+        // let tracks = load_tracks(&self.storage);
+        let tracks_column = column(elements).spacing(1);
+
         let arranger = container(
             scrollable(
-                column!["Track list"]
+                column!["Track list", tracks_column]
                     .spacing(40)
                     .align_items(Alignment::Center)
                     .width(Length::Fill),
---
+++ gui/src/widget/track.rs
@@ -0,0 +1,400 @@
+use hexencer_core::{
+    data::{ClipId, StorageInterface},
+    TrackId,
+};
+use iced::{
+    advanced::{
+        layout, mouse,
+        overlay::{self, from_children},
+        renderer::{self, Quad},
+        widget::{self, Tree},
+        Layout, Widget,
+    },
+    alignment, event,
+    theme::palette,
+    Background, Border, Color, Element, Event, Length, Padding, Rectangle, Shadow, Size, Theme,
+    Vector,
+};
+use tracing::info;
+
+/// handler for on track drop events
+type DropHandler<'a, Message> = Option<Box<dyn Fn(ClipId, TrackId, f32) -> Message + 'a>>;
+
+pub struct Track<'a, Message, Theme, Renderer>
+where
+    Theme: Catalog,
+    Renderer: renderer::Renderer,
+{
+    /// track id this widget represents
+    track_id: TrackId,
+    /// The padding of the track.
+    padding: Padding,
+    /// The width of the track.
+    width: Length,
+    /// The height of the track.
+    height: Length,
+    /// The maximum width of the track.
+    max_width: f32,
+    /// The maximum height of the track.
+    max_height: f32,
+    /// The horizontal alignment of the track.
+    horizontal_alignment: alignment::Horizontal,
+    /// The vertical alignment of the track.
+    vertical_alignment: alignment::Vertical,
+    /// The style of the track.
+    class: Theme::Class<'a>,
+    /// Is the track hovered?
+    hovered: bool,
+    /// The storage interface for the track.
+    storage: &'a StorageInterface,
+    /// The index of the track.
+    track_index: usize,
+    /// The children of the track.
+    children: Vec<Element<'a, Message, Theme, Renderer>>,
+    /// The dropped clip.
+    dropped_clip: Option<ClipId>,
+    /// if something was dropped on this track
+    on_drop: DropHandler<'a, Message>,
+}
+
+impl<'a, Message, Theme, Renderer> Track<'a, Message, Theme, Renderer>
+where
+    Renderer: renderer::Renderer,
+    Theme: Catalog,
+{
+    pub(crate) fn new(
+        storage: &'a hexencer_core::data::StorageInterface,
+        index: usize,
+        track_id: TrackId,
+        children: Vec<Element<'a, Message, Theme, Renderer>>,
+        dropped_clip: Option<ClipId>,
+    ) -> Self {
+        Self {
+            storage,
+            track_index: index,
+            track_id,
+            dropped_clip,
+            on_drop: None,
+            padding: Padding::ZERO,
+            width: Length::Fill,
+            height: Length::Fixed(18.0),
+            max_width: f32::INFINITY,
+            max_height: f32::INFINITY,
+            horizontal_alignment: alignment::Horizontal::Center,
+            vertical_alignment: alignment::Vertical::Top,
+            class: Theme::default(),
+            hovered: false,
+            children,
+        }
+    }
+
+    /// takes a closure for when something is dropped on this track
+    pub fn on_drop<F>(mut self, f: F) -> Self
+    where
+        F: 'a + Fn(ClipId, TrackId, f32) -> Message,
+    {
+        self.on_drop = Some(Box::new(f));
+        self
+    }
+
+    /// draws the track background    
+    fn draw_background(
+        &self,
+        _storage: std::sync::RwLockReadGuard<hexencer_core::data::DataLayer>,
+        _tree: &widget::Tree,
+        theme: &Theme,
+        renderer: &mut Renderer,
+        layout: Layout,
+        _cursor: mouse::Cursor,
+    ) {
+        let size = layout.bounds().size();
+
+        let bounds = layout.bounds();
+        let quad = Quad {
+            bounds: Rectangle {
+                x: bounds.x,
+                y: bounds.y,
+                width: size.width,
+                height: size.height,
+            },
+            border: Border::default(),
+            shadow: Shadow::default(),
+        };
+
+        let appearance = theme.style(&self.class, Status::Active);
+
+        if self.hovered {
+            renderer.fill_quad(quad, Background::Color(appearance.background_hovered));
+        } else {
+            renderer.fill_quad(
+                renderer::Quad {
+                    bounds: layout.bounds(),
+                    border: Border::default(),
+                    ..renderer::Quad::default()
+                },
+                appearance.background.unwrap(),
+            );
+        }
+    }
+}
+
+impl<'a, Message, Theme, Renderer> Widget<Message, Theme, Renderer>
+    for Track<'a, Message, Theme, Renderer>
+where
+    Message: 'a,
+    Theme: 'a + Catalog,
+    Renderer: 'a + renderer::Renderer,
+{
+    fn children(&self) -> Vec<Tree> {
+        self.children.iter().map(Tree::new).collect()
+    }
+
+    fn diff(&self, tree: &mut Tree) {
+        tree.diff_children(&self.children);
+    }
+
+    fn size(&self) -> Size<Length> {
+        Size {
+            width: Length::Shrink,
+            height: Length::Shrink,
+        }
+    }
+
+    fn layout(
+        &self,
+        tree: &mut widget::Tree,
+        renderer: &Renderer,
+        limits: &layout::Limits,
+    ) -> layout::Node {
+        let size = limits.resolve(self.width, self.height, Size::ZERO);
+
+        let children = self
+            .children
+            .iter()
+            .zip(&mut tree.children)
+            .map(|(child, child_tree)| child.as_widget().layout(child_tree, renderer, limits))
+            .collect();
+
+        layout::Node::with_children(size, children)
+    }
+
+    fn draw(
+        &self,
+        tree: &widget::Tree,
+        renderer: &mut Renderer,
+        theme: &Theme,
+        style: &renderer::Style,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+    ) {
+        let storage = self.storage.read().unwrap();
+        self.draw_background(storage, tree, theme, renderer, layout, cursor);
+
+        for ((child, tree), child_layout) in self
+            .children
+            .iter()
+            .zip(&tree.children)
+            .zip(layout.children())
+        {
+            child
+                .as_widget()
+                .draw(tree, renderer, theme, style, child_layout, cursor, viewport);
+        }
+    }
+
+    fn on_event(
+        &mut self,
+        tree: &mut widget::Tree,
+        event: iced::Event,
+        layout: Layout<'_>,
+        cursor: iced::advanced::mouse::Cursor,
+        renderer: &Renderer,
+        clipboard: &mut dyn iced::advanced::Clipboard,
+        shell: &mut iced::advanced::Shell<'_, Message>,
+        viewport: &Rectangle,
+    ) -> event::Status {
+        let bounds = layout.bounds();
+        if let Some(cursor_position) = cursor.position_in(bounds) {
+            if let Some(on_drop) = &self.on_drop {
+                if let Some(clip_id) = self.dropped_clip {
+                    info!("clip {} was dropped on {:?}", clip_id, self.track_id);
+                    self.dropped_clip = None;
+
+                    let pos = bounds.position();
+                    info!("track position: {:?}", pos);
+                    info!("cursor position: {:?}", cursor_position.x);
+                    let test = cursor_position.x - pos.x;
+                    info!("test: {:?}", test);
+                    shell.publish(on_drop(clip_id, self.track_id, cursor_position.x));
+                    return event::Status::Captured;
+                }
+            }
+        }
+
+        let child_events = self
+            .children
+            .iter_mut()
+            .zip(&mut tree.children)
+            .zip(layout.children())
+            .map(|((child, state), layout)| {
+                child.as_widget_mut().on_event(
+                    state,
+                    event.clone(),
+                    layout,
+                    cursor,
+                    renderer,
+                    clipboard,
+                    shell,
+                    viewport,
+                )
+            })
+            .fold(event::Status::Ignored, event::Status::merge);
+        let track_event = match event {
+            Event::Mouse(mouse::Event::CursorMoved { .. }) => {
+                let bounds = layout.bounds();
+                if let Some(_position) = cursor.position_in(bounds) {
+                    if !self.hovered {
+                        self.hovered = true;
+                        return event::Status::Captured;
+                    }
+                } else if self.hovered {
+                    self.hovered = false;
+                    return event::Status::Captured;
+                }
+                event::Status::Ignored
+            }
+            _ => event::Status::Ignored,
+        };
+        child_events.merge(track_event)
+    }
+
+    fn overlay<'b>(
+        &'b mut self,
+        tree: &'b mut Tree,
+        layout: Layout<'_>,
+        renderer: &Renderer,
+        translation: Vector,
+    ) -> Option<overlay::Element<'b, Message, Theme, Renderer>> {
+        from_children(&mut self.children, tree, layout, renderer, translation)
+    }
+
+    fn mouse_interaction(
+        &self,
+        tree: &Tree,
+        layout: Layout<'_>,
+        cursor: mouse::Cursor,
+        viewport: &Rectangle,
+        renderer: &Renderer,
+    ) -> mouse::Interaction {
+        self.children
+            .iter()
+            .zip(&tree.children)
+            .zip(layout.children())
+            .map(|((child, state), layout)| {
+                child
+                    .as_widget()
+                    .mouse_interaction(state, layout, cursor, viewport, renderer)
+            })
+            .max()
+            .unwrap_or_default()
+    }
+}
+
+impl<'a, Message, Theme, Renderer> From<Track<'a, Message, Theme, Renderer>>
+    for Element<'a, Message, Theme, Renderer>
+where
+    Message: 'a,
+    Theme: 'a + Catalog,
+    Renderer: 'a + renderer::Renderer,
+{
+    fn from(track: Track<'a, Message, Theme, Renderer>) -> Self {
+        Self::new(track)
+    }
+}
+
+/// The possible status of a [`Button`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Status {
+    /// The [`Button`] can be pressed.
+    Active,
+    /// The [`Button`] can be pressed and it is being hovered.
+    Hovered,
+    /// The [`Button`] is being pressed.
+    Pressed,
+    /// The [`Button`] cannot be pressed.
+    Disabled,
+}
+
+/// A primary button; denoting a main action.
+pub fn primary(theme: &Theme, status: Status) -> Style {
+    let palette = theme.extended_palette();
+    let base = styled(palette.primary.strong);
+
+    match status {
+        Status::Active | Status::Pressed => base,
+        Status::Hovered => Style {
+            background: Some(Background::Color(palette.primary.weak.color)),
+            ..base
+        },
+        Status::Disabled => disabled(base),
+    }
+}
+
+impl Catalog for Theme {
+    type Class<'a> = StyleFn<'a, Self>;
+
+    fn default<'a>() -> Self::Class<'a> {
+        Box::new(primary)
+    }
+
+    fn style(&self, class: &Self::Class<'_>, status: Status) -> Style {
+        class(self, status)
+    }
+}
+
+/// The theme catalog of a [`Button`].
+pub trait Catalog {
+    /// The item class of the [`Catalog`].
+    type Class<'a>;
+
+    /// The default class produced by the [`Catalog`].
+    fn default<'a>() -> Self::Class<'a>;
+
+    /// The [`Style`] of a class with the given status.
+    fn style(&self, class: &Self::Class<'_>, status: Status) -> Style;
+}
+
+/// A styling function for a [`Button`].
+pub type StyleFn<'a, Theme> = Box<dyn Fn(&Theme, Status) -> Style + 'a>;
+
+/// The appearance of a button.
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Style {
+    /// The [`Background`] of the button.
+    pub background: Option<Background>,
+    /// The text [`Color`] of the button.
+    pub text_color: Color,
+    /// The [`Background`] of the button.
+    pub clip_color: Color,
+    /// The hovered color
+    pub background_hovered: Color,
+}
+
+fn styled(pair: palette::Pair) -> Style {
+    Style {
+        background: Some(Background::Color(pair.color)),
+        text_color: pair.text,
+        ..Style::default()
+    }
+}
+
+fn disabled(style: Style) -> Style {
+    Style {
+        background: style
+            .background
+            .map(|background| background.scale_alpha(0.5)),
+        text_color: style.text_color.scale_alpha(0.5),
+        ..style
+    }
+}
