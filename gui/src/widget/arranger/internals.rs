30343c44daa756909f61c990016fb0ded0c7e415
wanja.svasek@emglive.com <wanja.svasek@emglive.com>
2024-06-23 22:52:27 +0200

    feat: Add arranger, clip, and track widgets
    This commit adds the arranger, clip, and track widgets to the GUI. The arranger widget is a new addition, while the clip and track widgets were already present but have been modified. The arranger widget is responsible for arranging clips and tracks in the GUI. The clip widget handles individual clips, and the track widget represents a track in the GUI. These changes are necessary for implementing new features related to clip manipulation and track management.

---
+++ gui/src/widget/arranger/internals.rs
@@ -0,0 +1,37 @@
+use iced::{Point, Rectangle};
+
+use super::Alignment;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Scrollbar {
+    pub total_bounds: Rectangle,
+    pub bounds: Rectangle,
+    pub scroll_handle: ScrollHandle,
+    pub alignment: Alignment,
+}
+
+impl Scrollbar {
+    /// Returns whether the mouse is over the scrollbar or not.
+    pub fn is_mouse_over(&self, cursor_position: Point) -> bool {
+        self.total_bounds.contains(cursor_position)
+    }
+
+    /// Returns the x-axis scrolled percentage from the cursor position.
+    pub fn scroll_percentage(&self, grabbed_at: f32, cursor_position: Point) -> f32 {
+        let percentage =
+            (cursor_position.x - self.bounds.x - self.scroll_handle.bounds.width * grabbed_at)
+                / (self.bounds.width - self.scroll_handle.bounds.width);
+
+        match self.alignment {
+            Alignment::Start => percentage,
+            Alignment::End => 1.0 - percentage,
+        }
+    }
+}
+
+/// The handle of a [`Scrollbar`].
+#[derive(Debug, Clone, Copy)]
+pub struct ScrollHandle {
+    /// The bounds of the [`Scroller`].
+    pub bounds: Rectangle,
+}
